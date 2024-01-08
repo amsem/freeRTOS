788ff06ebee9992569eff0ab5e57ae06f5f3c35e
amsem <90044743+amsem@users.noreply.github.com>
2024-01-08 12:34:34 +0100

    add: wifi_connect.c & remove wifi_sta

---
+++ WIFI_HTTP/wifi_connect.c
@@ -0,0 +1,58 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#define WIFI_SSID "Free"
+#define WIFI_PASS "123456789**"
+
+static const char *TAG = "wifi station";
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_id == WIFI_EVENT_STA_CONNECTED)
+    {
+        printf("Connected to AP\n");
+    }
+}
+
+void wifi_init_sta(void)
+{
+    nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             WIFI_SSID, WIFI_PASS);
+}
+
+void app_main(void)
+{
+    wifi_init_sta();
+}
